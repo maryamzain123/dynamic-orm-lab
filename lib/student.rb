c379800c41725baddf264ebf1dd352df21154420
maryamzain123 <smaryam.zain@gmail.com>
2021-03-29 16:31:06 -0500

    commit tree 69898b4679016255be47c9ee3fff2e4840d333d2

--- lib/student.rb
+++ lib/student.rb
@@ -4,4 +4,20 @@ require 'interactive_record.rb'
 
 class Student < InteractiveRecord
 
+ATTRIBUTES = {
+    :id => "INTEGER PRIMARY KEY",
+    :name =>"TEXT",
+    :grade => "TEXT"
+}
+def attr_accessor
+ATTRIBUTES.keys.each do |key|
+    attr_accessor key
+end
+end
+
+    def self.table_name
+        "#{self.to_s.downcase}s"
+    end
+
+
 end
